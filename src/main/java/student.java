c7af561403ae75b48941f0af24b8fef258aa5108
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-11-02 16:58:29 -0500

    studentTests

---
+++ src/main/java/student.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public student(long id, String name, ArrayList<Integer> grades) {
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+    }
+    //returns the student's id
+    public long getId() {
+        return id;
+    }
+
+    //returns the student's name
+    public String getName() {
+        return name;
+    }
+
+    //adds the given grade to the grades list
+    public void addGrade(Integer grade) {
+        grades.add(grade);
+    }
+
+    //returns the list of grades
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    //returns the average of the students grades
+    public double getGradeAverage() {
+        double sum = 0;
+        for (double grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
+}
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,45 +1,18 @@
+import org.junit.Test;
 import java.util.ArrayList;
 
+
 public class StudentTest {
-    private long id;
-    private String name;
-    private ArrayList<Integer> grades;
-
-    public StudentTest(long id, String name, ArrayList<Integer> grades) {
-        this.id = id;
-        this.name = name;
-        this.grades = grades;
-    }
-    //returns the student's id
-    public long getId() {
-        return id;
-    }
-
-    //returns the student's name
-    public String getName() {
-        return name;
-    }
-
-    //adds the given grade to the grades list
-    public void addGrade(Integer grade) {
-        grades.add(grade);
-    }
-
-    //returns the list of grades
-    public ArrayList<Integer> getGrades() {
-        return grades;
-    }
-
-    //returns the average of the students grades
-    public double getGradeAverage() {
-        double sum = 0;
-        for (double grade : grades) {
-            sum += grade;
-        }
-        return sum / grades.size();
-    }
-
-    public StudentTest() {
+    ArrayList<Integer> grades = new ArrayList<>();
+    @Test
+    public void student() {
+        student student1 = new student(1, "John", grades);
+        student1.addGrade(100);
+        student1.addGrade(90);
+        student1.addGrade(80);
+        student1.addGrade(70);
+        student1.addGrade(60);
+        student1.getGradeAverage();
     }
 
 }
